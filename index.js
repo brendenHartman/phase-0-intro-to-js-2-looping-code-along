bfea8f7f340e38618fae13da5bb36359e9bb29f0
Brenden Hartman <brendenhart22@gmail.com>
2023-11-28 01:44:48 -0600

    I loved working on this lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { getModulesPluginNames } = require("@babel/preset-env");
+
 // Code your solutions in this file
+function writeCards(names ,purp) {
+    let messages = [];
+    for(let i = 0; i < names.length; i++) {
+        messages[i] = "Thank you, " + names[i] + ", for the wonderful " + purp + " gift!";
+    }
+    return messages;
+}
+
+function countDown(n) {
+    while(n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
